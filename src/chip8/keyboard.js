0d869a91ee5f8448baa35a0c07c5e51f80292179
Renan-G-projec <renanfartes@gmail.com>
2026-07-11 12:05:41 -0300

    add: Keyboard release and click detecting. AMDG

---
+++ src/chip8/keyboard.js
@@ -0,0 +1,36 @@
+// Ad Maiorem Dei Gloriam!
+
+export default class Keyboard {
+    constructor() {
+        this.keyState = new Array(16);
+
+        this.addDOMListeners();
+    }
+    
+    addDOMListeners() {
+        const keyboard = document.getElementById("keyboard-keys");
+        let currentKeyId;
+        keyboard.addEventListener("mousedown", (ev) => {
+            const target = ev.target;
+            if (target.tagName.toLowerCase() !== "button") return;
+            
+            const keyPressedId = Number.parseInt(target.innerText.toLowerCase(), 16);
+            this.keyState[keyPressedId] = true;
+            
+            currentKeyId = keyPressedId;
+            console.log(`Key ${keyPressedId} pressed!`)
+
+        });
+        document.addEventListener("mouseup", (ev) => {
+            if (this.keyState[currentKeyId]) console.log(`Key ${currentKeyId} released!`)
+            this.keyState[currentKeyId] = false;
+        });
+        
+    }
+    
+    isKeyPressed(keyId) {
+
+    }
+
+
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import Keyboard from './chip8/keyboard.js'
+
 const app = createApp(App)
 
 app.mount('#app')
+
+const kb = new Keyboard();
