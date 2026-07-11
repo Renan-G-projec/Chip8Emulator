cae2c47ecd90db41637315a02eefa6f5e32d521d
Renan-G-projec <renanfartes@gmail.com>
2026-07-11 18:38:56 -0300

    feat: Sprite rendering on canvas. AMDG

--- src/chip8/canvas.js
+++ src/chip8/canvas.js
@@ -21,15 +21,39 @@ export default class Canvas {
     }
 
     /**
+     * @description Draws an 8 pixel wide square in the screen
+     * @returns {Boolean} If any pixel was flipped
+     */
+    drawSprite(posX, posY, height, uint8array) {
+        const getindex = function (x, y) { return y * this.width + height };
+        const setPixel = function (x, y, val) { this.data[getindex(x, y)] = val}
+
+        let changed = false;
+
+        for (let currentRow = 0; currentRow < height; currentRow++) {
+            for (let currentCol = 0; currentCol < 8; currentCol++) {
+                const currentX = posX + currentCol;
+                const currentY = posY + currentRow;
+
+                const bit = (uint8array[currentRow] & (1 << currentCol)) >> currentCol;
+                if (bit != this.data[getindex(currentX, currentY)]) changed = true;
+                setPixel(currentX, currentY, bit);
+            }
+        }
+
+        return changed;
+    }
+
+    /**
      * @description Draws the frambuffer to the screen.
      */
     render() {
         this.canvasContext.reset();
 
         this.canvasContext.fillStyle = this.fillColor;
-        for (let x = 0; x < this.width; x++) {
-            for (let y = 0; y < this.height; y++) {
-                if (this.data[x * this.height + y] > 0) this.canvasContext.fillRect(x, y, 1, 1);
+        for (let y = 0; y < this.height; y++) {
+            for (let x = 0; x < this.width; x++) {
+                if (this.data[y * this.width + x] > 0) this.canvasContext.fillRect(x, y, 1, 1);
             }
         }
     }
