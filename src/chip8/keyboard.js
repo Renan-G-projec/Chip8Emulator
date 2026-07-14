74298f60a8267161664ed53b36c3d45cc14ac45f
Renan-G-projec <renanfartes@gmail.com>
2026-07-14 07:46:45 -0300

    feat: Added Keyboard keypressing. AMDG

--- src/chip8/keyboard.js
+++ src/chip8/keyboard.js
@@ -4,7 +4,10 @@ export default class Keyboard {
     constructor() {
         this.keyState = new Array(16);
 
+        this.keyMapping = "1234qwerasdfzxcv";
+
         this.addDOMListeners();
+        this.addKeyboardDOMListeners();
     }
     
     addDOMListeners() {
@@ -26,6 +29,24 @@ export default class Keyboard {
             this.keyState[currentKeyId] = false;
         });
         
+    }
+
+    addKeyboardDOMListeners() {
+        document.addEventListener("keydown", (ev) => {
+            const key = this.keyMapping.indexOf(ev.key.toLowerCase());
+            if (key) {
+                this.keyState[key] = true;
+            }
+            console.log(`Key ${key} pressed!`)
+        })
+        document.addEventListener("keyup", (ev) => {
+            const key = this.keyMapping.indexOf(ev.key.toLowerCase());
+            if (key) {
+                this.keyState[key] = false;
+            }
+
+            console.log(`Key ${key} released!`)
+        })
     }
     
     isKeyPressed(keyId) {
