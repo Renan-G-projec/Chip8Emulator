066ec23e13fd7728aef7ecb8b23583071a30aed2
Renan-G-projec <renanfartes@gmail.com>
2026-07-14 08:49:05 -0300

    refactor: Mived Screen to a separated Component. AMDG

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,22 @@
     import Keyboard from './components/Keyboard.vue';
 
     import FileInput from './components/FileInput.vue';
+    import Screen from './components/Screen.vue';
 </script>
 
 <template>
   <div class="game-viewport">
     <Window title="Game" img="Game" class="game-window">
-      <canvas id="main-canvas" width="64" height="32"></canvas> <br>
+      <Screen/>
       <button id="init-game-btn">Init Game</button>
     </Window>
     
     <Keyboard class="keyboard"/>
   </div>
 
-
   <FileInput/>
+
+  <Console/>
 </template>
 
 <style scoped>
@@ -28,18 +30,6 @@
       align-items: stretch;
     }
 
-    #main-canvas {
-      background-color: black;
-      border-radius: 0.2rem;
-      width: 45vw;
-      height: calc(45vw / 2);
-
-      image-rendering: pixelated;
-
-      margin: 0.4rem 0.4rem 0.4rem 0.4rem;
-      vertical-align: bottom;
-    }
-
     .keyboard {
       width: 30vw;
       justify-self: stretch;
@@ -48,9 +38,6 @@
     @media (width < 720px) {
       .game-viewport {
         flex-direction: column;
-      }
-      #main-canvas {
-        width: 80vw;
       }
     }
 </style>
---
+++ src/components/Screen.vue
@@ -0,0 +1,25 @@
+<!-- Ad Maiorem Dei Gloriam!-->
+
+<template>
+    <canvas id="main-canvas" width="64" height="32"></canvas> <br>
+</template>
+
+<style scoped>
+    #main-canvas {
+        background-color: black;
+        border-radius: 0.2rem;
+        width: 20rem;
+        height: calc(20rem / 2);
+
+        image-rendering: pixelated;
+
+        margin: 0.4rem 0.4rem 0.4rem 0.4rem;
+        vertical-align: bottom;
+    }
+
+    @media (width < 720px) {
+        #main-canvas {
+            width: 80vw;
+        }
+    }
+</style>
