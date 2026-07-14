75389025b38efa56163c36d6bd2ddb6ecf32b7cf
Renan-G-projec <renanfartes@gmail.com>
2026-07-14 10:58:08 -0300

    refactor: Improved centralization in file input component. AMDG

--- src/components/FileInput.vue
+++ src/components/FileInput.vue
@@ -5,12 +5,37 @@
 </script>
 
 <template> 
-    <Window title="Load ROM" img="a">
-        <input type="file" id="rom-file-input">
-        <div>
+    <Window title="Load ROM" img="a" class="load-file-window">
+        <div class="container">
+            <input type="file" id="rom-file-input">
             <button id="load-rom-btn">Load ROM</button>
         </div>
     </Window>
 </template>
 
-<style></style>
+<style scoped>
+    button, input {
+        font-family: "Pixelify Sans", sans-serif;
+        border: none;
+        background-color: transparent;
+        color: white;
+
+        cursor: pointer;
+    }
+
+    button:hover {
+        color: grey;
+    }
+
+    .container {
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+
+        justify-content: center;
+    }
+
+    .load-file-window div {
+        padding: 1rem;
+    }
+</style>
