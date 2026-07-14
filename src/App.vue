34e65f17cc5492e84533fdd8caf2943a7e23215e
Renan-G-projec <renanfartes@gmail.com>
2026-07-14 10:45:46 -0300

    add: Hero component added. AMDG

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,13 @@
 <script setup>
-    import Window from './components/Window.vue';
-    import Keyboard from './components/Keyboard.vue';
-
     import FileInput from './components/FileInput.vue';
-    import Screen from './components/Screen.vue';
     import Console from './components/Console.vue';
+    import Hero from './components/Hero.vue';
 </script>
 
 <template>
-  <div class="game-viewport">
-    <Console/>
-  </div>
+  <Hero/>
+
+  <Console/>
 
   <FileInput/>
 </template>
-
-<style scoped>
-
-    .game-viewport {
-      display: flex;
-      justify-content: space-around;
-      height: fit-content;
-      align-items: stretch;
-    }
-
-    @media (width < 720px) {
-      .game-viewport {
-        flex-direction: column;
-      }
-    }
-</style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,40 @@
+<!-- Ad Maiorem Dei Gloriam! -->
+
+<script setup>
+    import { onMounted, ref } from 'vue';
+    const heroText = "Chip 8 Emulator";
+    const heroString = ref("");
+    const delay = 40;
+
+    function incrementChar() {
+        if (heroString.value.length < heroText.length) {
+            heroString.value += heroText.charAt(heroString.value.length);
+        }
+    }
+
+    onMounted(() => {
+        const interval = setInterval(() => {
+            incrementChar();
+            if (heroString.length >= heroText.length) clearInterval(interval);
+        }, delay)
+    })
+</script>
+
+<template>
+    <div class="hero">
+        <h1>{{  heroString }}</h1>
+    </div>
+</template>
+
+<style scoped>
+
+    .hero {
+        text-align: center;
+    }
+    
+    .hero h1 {
+        font-family: "Pixelify Sans", sans-serif;
+        font-size: 4rem;
+        color: white;
+    }
+</style>
